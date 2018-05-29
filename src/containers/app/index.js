8353b8166726e7a11ed88e0a1b88e84900a1367b
Andrey Brunetkin <brunetkin@reg.ru>
2018-05-29 12:46:04 +0500

    remove react-split-pane & comments

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,28 +1,11 @@
 import React from 'react';
 
-// import { Route, Link } from 'react-router-dom';
-// import Home from '../home';
-// import Map from '../about';
 
 import Time from '../../modules/time';
 import AutoFitImage from '../../modules/autofit';
 import Icon from '../../modules/icon';
 
-import SplitPane from 'react-split-pane';
-
-// import fontawesome from '@fortawesome/fontawesome';
-// import brands from '@fortawesome/fontawesome-free-brands';
-
 import img from '../../../images/land.jpg';
-
-// import img from 'https://source.unsplash.com/1920x1200/?land';
-
-// let q = fontawesome.icon(brands.faVk).html;
-
-debugger;
-
-// Запрет/разрешение ресайза ячеек
-const ALLOW_RESIZE = false;
 
 const App = () => (
   <main style={ { height: '100vh' } }>
@@ -41,10 +24,6 @@ const App = () => (
 
       <div />
     </div>
-
-
-
-
   </main>
 );
 
--- src/modules/Icon.js
+++ src/modules/Icon.js
@@ -9,22 +9,6 @@ export default class Icon extends React.Component {
         this.state = { svgIcon: fontawesome.icon(brands.faVk).html };
     }
 
-    /* currentTime() {
-        return moment().format('HH:mm');
-    }
-
-    tick() {
-        this.setState(() => ({ time: this.currentTime() }));
-    }
-
-    componentDidMount() {
-        this.interval = setInterval(() => this.tick(), 2000);
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.interval);
-    } */
-
     render() {
         return (
             <div dangerouslySetInnerHTML={{ __html: this.state.svgIcon }} />
